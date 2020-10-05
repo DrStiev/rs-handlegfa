e70edc41ccfaea89670bf889d4c47be2b2d6813d
DrStiev <55959491+DrStiev@users.noreply.github.com>
2020-10-05 12:32:41 +0200

    update
    - add a write_file function
    - done some refactoring
    - separate the main code from the test code

--- gfa_to_handlegraph/src/main.rs
+++ gfa_to_handlegraph/src/main.rs
@@ -19,6 +19,8 @@ use std::io::prelude::*;
 use std::{io, env};
 extern crate glob;
 use glob::glob;
+/// library to access the path of a file
+use std::path::Path;
 
 // (october 4, 2020)
 // TODO: find a way to display either the content (or the error) and the name of the file
@@ -63,16 +65,44 @@ fn read_file(filename: &str) -> Result<String, io::Error> {
     Ok(content)
 }
 
+/// a function to control if a the file created by write_file exist or not
+/// (october 5, 2020) 
+fn check_file_exist(result: Result<bool, io::Error>) {
+    match result {
+        Ok(_) => println!("File created successfully!"),
+        Err(why) => println!("Error: {}", why),
+    }
+}
+
+/// a function that create a file and then write a string of information in it
+/// (october 5, 2020)
+fn write_file(filename: &str, content: &str) -> Result<bool, io::Error> {
+    let path = Path::new(filename);
+
+    // try to open a file in write-only mode, returns io::Result<File>
+    let mut file = File::create(&path)?; 
+
+    // write in file the string content passed as input parameter
+    file.write_all(content.as_bytes())?;
+
+    Ok(file.metadata().unwrap().is_file())
+}
+
 /// a function to print the result of the function read_directory_files
 /// (october 4, 2020)
-fn print_dir(dircontent: Vec<String>) {
-    if dircontent.is_empty() {
-        println!("The directory is empty");
-    } else {
-        for file in dircontent {
-            println!("{}", file);
-        }
-    }
+fn print_dir(dircontent: Result<Vec<String>, io::Error>) {
+    match dircontent{
+        Ok(dir) => {
+            if dir.is_empty() {
+                println!("The directory is empty")
+            } else {
+                for file in dir {
+                    println!("{}", file)
+                }
+            }
+        },
+        Err(why) => println!("Error: {}\n", why),
+    }  
 }
 
 //  (october 4, 2020)
@@ -83,22 +113,19 @@ fn print_dir(dircontent: Vec<String>) {
 ///            |- fooFile2.txt
 /// the function will return: result = ["fooFile.txt", "fooFile2.txt"]
 /// (october 3, 2020)
-fn read_directory_files(dirname: &str) -> Vec<String> {
+fn read_directory_files(dirname: &str) -> Result<Vec<String>, io::Error> {
     // declare a Vec<String> variable to store the results
     let mut files:Vec<String> = vec![];
 
     // Search through the entire directory with a loop
     // nested directory will be returned as if they are plain file
-    for e in glob(dirname).unwrap() {
+    for e in glob(dirname).expect("Failed to read glob pattern") {
         match e {
             // if match gives a positive result, that means the directory exist,
-            // go and store the name of the file found in it
-            Ok(path) => {
-                // declare a variable to store the name of the file found over each iteration
-                let filename = path.display().to_string();
-                // insert the name of the file into the Vec<String> variable "files"
-                files.push(filename)
-            },
+            // go and store the name of the file (or sub-directory) found in it
+            // insert the name of the file (or sub-directory) into the Vec<String> variable "files"
+            Ok(path) => files.push(path.display().to_string()),
+                 
             // this branch of the match statement seems to not working properly.
             // It should catch the error if a non-existing directory is passed as input and then
             // display an error message, but the error message it's never displayed
@@ -109,120 +136,55 @@ fn read_directory_files(dirname: &str) -> Vec<String> {
     // return the vector containing the result produced by the function
     // a return expression DO NOT want the character ";" at the end of it
     // otherwise the function will return its default value ()
-    files
+    Ok(files)
 }
 
 /// function help that inform the user on how the program works and how to use it in the right way
 /// (october 4, 2020)
-fn help() {
-    println!("The point of this project is to implement the rs-handlegraph interface to handling \
-    genomic graphs that are being represented via the library rs-gfa.\n\
-    Both of the libraries are developed by Christian Fischer and are available on GitHub\n");
+fn help(otpional_parameters: Option<usize>) {
+    // the only way to implement optional parameters and default parameter in a Rust program
+    // is to define an Option<usize> parameter in the target function
+    // this because Rust DOES NOT support default function argument
+    // (october 5, 2020)
+    if let Some(20) = otpional_parameters {
+        // print the help message relative to the error of BAD_OPERATION
+        // (october 5, 2020)
+        println!("The operation you can perform now are:\n\
+        1) with the command rf: read the content of a file \n\
+        2) with the command rd: display the content of a directory\n\
+        3) with the command rdf: display the content of a directory and read the content of each valid file in it\n");    
+    } else {
+        // print a generic help message
+        // (october 5, 2020)
+        println!("To run properly this application via command line you should do as follow:\n\
+        1) run the application with ./application_name argument1 argument2\n\
+        2) argument1 is referred to what kind of operation you are intended to do\n\
+        3) argument2 is referred to what kind of input, based on the operation you wanna perform, you \
+        wanna pass to the program\n");
 
-    println!("To run properly this application via command line you should do as follow:\n\
-    1) run the application with ./application_name argument1 argument2\n\
-    2) argument1 is referred to what kind of operation you are intended to do\n\
-    3) argument2 is referred to what kind of input, based on the operation you wanna perform, you \
-    wanna pass to the program\n");
+        println!("The operation you can perform now are:\n\
+        1) with the command rf: read the content of a file \n\
+        2) with the command rd: display the content of a directory\n\
+        3) with the command rdf: display the content of a directory and read the content of each valid file in it\n");
 
-    println!("The operation you can perform now are:\
-    1) with the command rf: read the content of a file \n\
-    2) with the command rd: display the content of a directory\n\
-    3) with the command rdf: display the content of a directory and read the content of each valid file in it\n");
-
-    println!("The input you can pass to the application is either:\n\
-    1) a path to a file\n\
-    2) a path to a directory\n")
+        println!("The input you can pass to the application is either:\n\
+        1) a path to a file\n\
+        2) a path to a directory\n")
+    }
 }
 
-/// implement various test function
-/// (october 4, 2020)
+/// separated the test.rs file from the main.rs file so the code can remain clean and readable
+/// (october 5, 2020)
 #[cfg(test)]
-mod tests{
-    use super::*;
-
-    /// implement test function for help
-    #[test]
-    fn test_help() {
-        help();
-    }
-
-    /// implement test function for read_file passing an existing file as input
-    #[test]
-    fn read_existing_file() {
-        println!("Test function \"read_file\" passing an existing file as input\n");
-        let temp_filepath = "test/gfas/gfa1_files/example1.gfa";
-        print(read_file(temp_filepath));
-    }
-
-    /// implement test function for read_file passing an existing empty file as input
-    #[test]
-    fn read_empty_file() {
-        println!("Test function \"read_file\" passing an existing empty file as input\n");
-        let temp_filepath = "test/gfas/gfa2_files/blankDocument.gfa";
-        print(read_file(temp_filepath));
-    }
-
-    /// implement test function for read_file passing an existing file >= 1MB as input
-    #[test]
-    fn read_big_file() {
-        println!("Test function \"read_file\" passing an existing file >= 1MB as input\n");
-        let temp_filepath = "test/gfas/big_file/very_big_file.gfa";
-        print(read_file(temp_filepath));
-    }
-
-    /// implement test function for read_file passing a non-existing file as input
-    #[test]
-    fn read_non_existing_file() {
-        println!("Test function \"read_file\" passing a non-existing file as input\n");
-        let temp_filepath = "test/gfas/i_dont_exist.gfa";
-        print(read_file(temp_filepath));
-    }
-
-    /// implement test function for read_file passing a directory instead of a file as input
-    #[test]
-    fn read_directory_as_file() {
-        println!("Test function \"read_file\" passing a directory instead of a file as input\n");
-        let temp_dirpath = "test/gfas";
-        print(read_file(temp_dirpath));
-    }
-
-    /// implement test function for read_directory_files passing an existing directory as input
-    #[test]
-    fn scan_existing_directory() {
-        println!("Test function \"read_directory_files\" passing an existing directory as input");
-        let temp_dir = "test/gfas/*";
-        println!("The directory {} contains the files:\n ", temp_dir);
-        print_dir(read_directory_files(temp_dir));
-    }
-
-    /// implement test function for read_directory_files passing an existing directory as input
-    /// testing the search for .gfa files in all the directory and subdirectories
-    #[test]
-    fn scan_existing_directory_recursively() {
-        println!("Test function \"read_directory_files\" passing an existing directory as input");
-        let temp_dir = "test/gfas/**/*.gfa";
-        println!("The directory {} and all its subdirectories, contain the files:\n ", temp_dir);
-        print_dir(read_directory_files(temp_dir));
-    }
-
-    /// implement test function for read_directory_files passing a non-existing directory as input
-    #[test]
-    fn scan_non_existing_directory() {
-        println!("Test function \"read_directory_files\" passing a non-existing directory as input");
-        let temp_dir = "test/i_dont_exist/*";
-        println!("The directory {} contains the files:\n ", temp_dir);
-        print_dir(read_directory_files(temp_dir));
-    }
-}
+mod test;
 
 fn main() {
 
     // REMEMBER! from path/project_folder cargo build to build a rust project
     //          from path/project_folder cargo run with_optional_argument to run a rust application
 
-    /// read the arguments passed as command line parameters
-    /// (october 4, 2020)
+    // read the arguments passed as command line parameters
+    // (october 4, 2020)
     let args: Vec<String> = env::args().collect();
 
     // check the length of the argument passed as command line parameters
@@ -249,12 +211,13 @@ fn main() {
                     // insert as input
                     let mut arg = argument.clone();
 
-                    if last_char == '/' {
-                        arg.push('*');
-                    } else {
-                        arg.push_str("/*");
+                    // changed control statement from if-else to match
+                    // (october 5, 2020) 
+                    match last_char {
+                        '/' => arg.push('*'),
+                        _ => arg.push_str("/*"),
                     }
-
+                  
                     print_dir(read_directory_files(&*arg))
                 },
 
@@ -263,31 +226,34 @@ fn main() {
                     let last_char = argument.chars().last().unwrap();
                     let mut arg = argument.clone();
 
-                    if last_char == '/' {
-                        arg.push('*');
-                    } else{
-                        arg.push_str("/*");
+                    // changed control statement from if-else to match
+                    // (october 5, 2020) 
+                    match last_char {
+                        '/' => arg.push('*'),
+                        _ => arg.push_str("/*"),
                     }
 
                     print_dir(read_directory_files(&*arg));
 
-                    let files = read_directory_files(&*arg);
-                    for file in files {
-                        print(read_file(&*file));
+                    let dir = read_directory_files(&*arg);
+                    for files in dir {
+                        for file in files {
+                            print(read_file(&file));
+                        }
                     }
                 },
 
                 _ => {
-                    eprintln!("Error! Invalid command\n");
-                    help();
+                    eprintln!("\nError! Invalid command\n");
+                    help(Some(20));
                 },
             }
         },
         // if the number of argument passed as command line parameters is not proper
         // the program will display an error message to inform the user
         _ => {
-            eprintln!("Error! The number of argument passed is not correct to run the program properly!\n");
-            help();
+            eprintln!("\nError! The number of argument passed is not correct to run the program properly!\n");
+            help(Some(0));
             return;
         },
     }
---
+++ gfa_to_handlegraph/src/test.rs
@@ -0,0 +1,103 @@
+/// implement various test function
+/// (october 4, 2020)
+use super::*;
+
+/// implement test function for help
+#[test]
+fn test_help() {
+    help(Some(0));
+}
+
+/// implement test function for read_file passing an existing file as input
+#[test]
+fn read_existing_file() {
+    println!("Test function \"read_file\" passing an existing file as input\n");
+    let temp_filepath = "test/gfas/gfa1_files/example1.gfa";
+    print(read_file(temp_filepath));
+}
+
+/// implement test function for read_file passing an existing empty file as input
+#[test]
+fn read_empty_file() {
+    println!("Test function \"read_file\" passing an existing empty file as input\n");
+    let temp_filepath = "test/gfas/gfa2_files/blankDocument.gfa";
+    print(read_file(temp_filepath));
+}
+
+/// implement test function for read_file passing an existing file >= 1MB as input
+#[test]
+fn read_big_file() {
+    println!("Test function \"read_file\" passing an existing file >= 1MB as input\n");
+    let temp_filepath = "test/gfas/big_file/very_big_file.gfa";
+    print(read_file(temp_filepath));
+}
+
+/// implement test function for read_file passing a non-existing file as input
+#[test]
+fn read_non_existing_file() {
+    println!("Test function \"read_file\" passing a non-existing file as input\n");
+    let temp_filepath = "test/gfas/i_dont_exist.gfa";
+    print(read_file(temp_filepath));
+}
+
+/// implement test function for read_file passing a directory instead of a file as input
+#[test]
+fn read_directory_as_file() {
+    println!("Test function \"read_file\" passing a directory instead of a file as input\n");
+    let temp_dirpath = "test/gfas";
+    print(read_file(temp_dirpath));
+}
+
+/// implement test function for write_file passing an existing file and path as input
+#[test]
+fn write_content_on_valid_file() {
+    println!("Test function \"write_file\" passing a valid file and a valid content\n");
+    let tempfile = "test/test_output_file/test_file.gfa";
+    let tempcontent = read_file("test/gfas/gfa2_files/example2.gfa").unwrap();
+    check_file_exist(write_file(tempfile, &tempcontent)); 
+}
+
+/// implement test function for write_file passing an existing file and path as input
+#[test]
+fn write_on_unnamed_file() {
+    println!("Test function \"write_file\" passing a valid file and a valid content\n");
+    let tempfile = "";
+    let tempcontent = read_file("test/gfas/gfa2_files/example2.gfa").unwrap();
+    check_file_exist(write_file(tempfile, &tempcontent)); 
+}
+
+/// implement test function for write_file passing an existing file and path as input
+#[test]
+fn write_empty_file() {
+    println!("Test function \"write_file\" passing a valid file and a valid content\n");
+    let tempfile = "test/test_output_file/empty_file.gfa";
+    check_file_exist(write_file(tempfile, "")); 
+}
+
+/// implement test function for read_directory_files passing an existing directory as input
+#[test]
+fn scan_existing_directory() {
+    println!("Test function \"read_directory_files\" passing an existing directory as input");
+    let temp_dir = "test/gfas/*";
+    println!("The directory {} contains the files:\n ", temp_dir);
+    print_dir(read_directory_files(temp_dir));
+}
+
+/// implement test function for read_directory_files passing an existing directory as input
+/// testing the search for .gfa files in all the directory and subdirectories
+#[test]
+fn scan_directory_recursively() {
+    println!("Test function \"read_directory_files\" passing an existing directory as input");
+    let temp_dir = "test/gfas/**/*.gfa";
+    println!("The directory {} and all its subdirectories, contain the files:\n ", temp_dir);
+    print_dir(read_directory_files(temp_dir));
+}
+
+/// implement test function for read_directory_files passing a non-existing directory as input
+#[test]
+fn scan_non_existing_directory() {
+    println!("Test function \"read_directory_files\" passing a non-existing directory as input");
+    let temp_dir = "test/i_dont_exist/*";
+    println!("The directory {} contains the files:\n ", temp_dir);
+    print_dir(read_directory_files(temp_dir));
+}
