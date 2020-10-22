bbf350049151bce4b000a91f98d7af80f0116585
DrStiev <55959491+DrStiev@users.noreply.github.com>
2020-10-22 14:16:33 +0200

    clean up

--- rs-gfa2/src/gfa2/name_conversion.rs
+++ rs-gfa2/src/gfa2/name_conversion.rs
@@ -498,13 +498,13 @@ mod tests {
     use crate::parser_gfa2::GFA2Parser;
 
     fn irl() -> &'static str {
-        "./tests/gfa2_files/irl.gfa"
+        "./src/tests/gfa2_files/irl.gfa"
     }
 
     fn load_irl() -> GFA2<BString, OptionalFields> {
         let parser = GFA2Parser::new();
         let gfa2 : GFA2<BString, OptionalFields> =
-            parser.parse_file(&"./tests/gfa2_files/irl.gfa").unwrap();
+            parser.parse_file(&"./src/tests/gfa2_files/irl.gfa").unwrap();
         gfa2
     }
 
